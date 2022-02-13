385106932aeeaceb04597cf33a1bc6b938e33b1f
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2022-02-13 14:33:38 -0300

    Add button component

---
+++ src/components/button/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Button.module.scss';
+
+function Button(props) {
+  const { children, ...buttonProps } = props;
+  return (
+    <button className={styles.button} {...buttonProps}>
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  type: 'button',
+};
+
+Button.propTypes = {
+  children: PropTypes.oneOfType(
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.node
+  ),
+  name: PropTypes.string,
+  type: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+export default Button;
