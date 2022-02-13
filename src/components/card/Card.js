c7b6178dea4ee86fb390f987207f7468f7aca0b1
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2022-02-13 16:43:17 -0300

    Add card component

---
+++ src/components/card/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Card.module.scss';
+
+function Card(props) {
+  return <div className={styles.card}>{props.children}</div>;
+}
+
+Card.propTypes = {
+  children: PropTypes.oneOfType(
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.node
+  ),
+};
+
+export default Card;
