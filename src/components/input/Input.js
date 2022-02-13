e039f922871ad3ae6bb18eecf18bfa6e96505c34
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2022-02-13 14:00:54 -0300

    Add input component

---
+++ src/components/input/Input.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import inputStyles from './Input.module.scss';
+
+function Input(props) {
+  const { label, prepend, ...inputProps } = props;
+  return (
+    <div className={inputStyles.input}>
+      <label htmlFor={inputProps.name}>{label}</label>
+      <div className={inputStyles['input-box']}>
+        {!!prepend && (
+          <span className={inputStyles['input-prepend']}>{prepend}</span>
+        )}
+        <input {...inputProps} />
+      </div>
+    </div>
+  );
+}
+
+Input.defaultValues = {
+  type: 'text',
+};
+
+Input.propTypes = {
+  label: PropTypes.string,
+  prepend: PropTypes.oneOfType(
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.node
+  ),
+  name: PropTypes.string,
+  value: PropTypes.string,
+  type: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+export default Input;
