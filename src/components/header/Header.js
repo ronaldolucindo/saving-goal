d04486e50c900fbe75857d2504c572dfe2b4a43d
Ronaldo Lucindo <13293540+ronaldolucindo@users.noreply.github.com>
2022-02-13 23:02:27 -0300

    Add mobile layout improvements

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import headerStyles from './Header.module.scss';
+import styles from './Header.module.scss';
 import Logo from 'assets/images/logo.png';
 
 function Header() {
   return (
-    <header className={headerStyles.header}>
-      <img src={Logo} alt="Origin" />
+    <header className={styles.header}>
+      <img src={Logo} className={styles.logo} alt="Origin" />
     </header>
   );
 }
